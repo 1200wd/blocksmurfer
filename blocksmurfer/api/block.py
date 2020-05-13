5317c300e6ffc053d0a620c8bcccdbf97609278d
Lennart <lennart@1200wd.com>
2020-05-13 10:14:56 +0200

    [REF] API for blocks: add attributes

--- blocksmurfer/api/block.py
+++ blocksmurfer/api/block.py
@@ -8,14 +8,20 @@ from blocksmurfer.explorer.service import *
 
 @bp.route('/<string:network>/block/<blockid>')
 def block(network, blockid):
+    parse_transactions = request.args.get('parse_transactions', False, type=str)
+    limit = request.args.get('limit', 10, type=int)
+    page = request.args.get('page', 1, type=int)
     srv = SmurferService(network)
     if blockid == 'last':
         blockid = srv.blockcount()
-    bk = srv.getblock(blockid, limit=5)
-    # bk = srv.getblock(blockid, parse_transactions=True, limit=5)
+    if parse_transactions:
+        bk = srv.getblock(blockid, parse_transactions=True, limit=limit, page=page)
+    else:
+        bk = srv.getblock(blockid)
     if not bk:
         return resp_error(404, message="Block not found")
-    # bk['txs'] = marshal(bk['txs'], transaction_fields)
+    if parse_transactions:
+        bk['txs'] = marshal(bk['txs'], transaction_fields)
     return bk
 
 
