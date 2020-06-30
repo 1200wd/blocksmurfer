1b348afb5143712c42a005ecc02981dbfb34ac51
Lennart <lennart@1200wd.com>
2020-06-30 22:13:39 +0200

    [REF] Simplify main search page

--- blocksmurfer/main/forms.py
+++ blocksmurfer/main/forms.py
@@ -4,5 +4,6 @@ from wtforms.validators import DataRequired
 
 
 class SearchForm(FlaskForm):
-    search = StringField('Address, transaction, block, wif, key', validators=[DataRequired()])
+    search = StringField('', validators=[DataRequired()],
+                         render_kw={"placeholder": "enter address, transaction, block id or hash, key, etc"})
     submit = SubmitField('Search')
