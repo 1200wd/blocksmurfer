e776fba1dd240f89da262f2d5591dacdb13d1212
Lennart <lennart@1200wd.com>
2020-07-08 21:24:06 +0200

    [REF] Provide better errors and too many requests errors

--- blocksmurfer/main/errors.py
+++ blocksmurfer/main/errors.py
@@ -9,23 +9,33 @@ _logger = logging.getLogger(__name__)
 
 @blueprint.app_errorhandler(Exception)
 def handle_errors(e):
-    try:
-        _logger.error(str(e))
-        name = e.get('name')
-        description = e.get('description', str(e))
-        code = e.get('code')
-        if '/api/' in request.path:
-            payload = {
-                'error': "API request error: %s" % name,
-                'description': description,
-                'code': code,
-            }
-            _logger.warning("Error %d: %s (%s). Request URL: %s" % (e.code, e.name, e.description, request.path))
-            resp = jsonify(payload)
-            resp.status_code = code
-            return resp, 500 if not code else code
-    except:
-        name = e
-        description = ''
-        code = 500
+
+    def getattr(object, attribute, default=''):
+        try:
+            return object.__getattribute__(attribute)
+        except:
+            try:
+                return object.get(attribute, default)
+            except:
+                return default
+
+    _logger.error(str(e))
+    name = getattr(e, 'name')
+    description = getattr(e, 'description', str(e))
+    code = getattr(e, 'code', 500)
+
+    if '/api/' in request.path:
+        if code == 429:
+            error_remark = "We are currently in a testing phase, so rate limits are set to low values. Please contact explorer@blocksmurfer.io if you would like to raise your limits"
+        else:
+            error_remark = "Please report errors and bugs to explorer@blocksmurfer.io. Thanks for your help!"
+        payload = {
+            'error': "API request error: %s. %s" % (name, error_remark),
+            'description': description,
+            'code': code,
+        }
+        _logger.warning("Error %d: %s (%s). Request URL: %s" % (e.code, e.name, e.description, request.path))
+        resp = jsonify(payload)
+        resp.status_code = code
+        return resp, 500 if not code else code
     return render_template("error.html", title=('Error: %s' % name), description=description, code=code), code
