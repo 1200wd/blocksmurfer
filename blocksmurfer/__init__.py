bee34647b431bc0ca66bb89b986472254ed36109
Lennart <lennart@1200wd.com>
2021-10-04 21:23:55 +0200

    [ADD] github, mail, api links

--- blocksmurfer/__init__.py
+++ blocksmurfer/__init__.py
@@ -49,7 +49,7 @@ def current_app(config_class=Config):
     babel.init_app(app)
     qrcode.init_app(app)
 
-    if Config.ENABLE_API:
+    if Config.ENABLE_API and isinstance(Config.ENABLE_API, bool):
         from blocksmurfer.api import bp as api_bp
         app.register_blueprint(api_bp, url_prefix='/api/v1')
         limiter.init_app(app)
