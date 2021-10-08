9297a3653aa53b2060f40ced8ccc194ca8e22ed8
Lennart <lennart@1200wd.com>
2021-10-08 21:03:20 +0200

    [REF] Improve live server settings

--- boot.sh
+++ boot.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
 source ~/venv/blocksmurfer/bin/activate
+export PYTHONWARNINGS="ignore:Unverified HTTPS request"
+
 #flask db upgrade
 #flask translate compile
+
 exec gunicorn -b localhost:5000 --access-logfile - --error-logfile - app:app --workers 4
 
