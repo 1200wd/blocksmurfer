385e372c0e011c6db93504ff7e7a74598f24e2f2
Cryp Toon <info@1200wd.com>
2024-09-10 19:25:04 +0200

    Add blocksmurfer stylesheet and copy raw tx button

---
+++ blocksmurfer/static/javainclude.js
@@ -0,0 +1,9 @@
+let text = document.getElementById('text2copy').value;
+const copyContent = async () => {
+    try {
+        await navigator.clipboard.writeText(text);
+        console.log('Content copied to clipboard');
+    } catch (err) {
+        console.error('Failed to copy: ', err);
+    }
+}
